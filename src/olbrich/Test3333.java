d3e3754f0f0c24f0a7378103b6e6eadc0ad25d04
molbrich <molbrich@student.tgm.ac.at>
2017-03-26 14:08:15 +0200

    hallo

---
+++ src/olbrich/Test3333.java
@@ -0,0 +1,12 @@
+package olbrich;
+
+public class Test3333 {
+
+	//git@github.com:molbricholbrich/test.git
+	public static void main(String[] args) {
+		System.out.println("Guten Morgen Hr. Olbrich");
+		System.out.println("Guten Morgen Hr. Olbrich");
+		System.out.println("Guten Morgen Hr. Olbrich");
+	}
+
+}
